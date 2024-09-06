e729a35b5f65be4891881f16ff487744b876250f
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-09-06 18:48:56 +0530

    Create Strong-password-checker.js

---
+++ Strong-password-checker.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} password
+ * @return {boolean}
+ */
+var strongPasswordCheckerII = function(password) {
+    let flag;
+    let lc=false;
+    let uc=false;
+    let sc=false;
+    let d=false;
+    let ac= true;
+    const specialChar="!@#$%^&*()-+";
+    if(password.length<8)
+    {
+        return false;
+    }
+    for(let i=0;i<password.length;i++)
+    {
+        let char=password[i];
+        if(char>='a' && char<='z')
+        {
+            lc=true;
+        }
+        else if(char>='A' && char<='Z')
+        {
+            uc=true;
+        }
+        else if(char >= '0' && char <= '9')
+        {
+            d=true;
+        }
+        else if(specialChar.includes(char))
+        {
+            sc=true;
+        }
+        else if(char != password[i-1])
+        {
+            return false;
+        }
+    }
+    return lc && uc && d && sc && ac;
+};
